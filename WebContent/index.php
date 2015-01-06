c18a403457bf58d025997f41f7b5336b1ee7ee23
son.dang <sondang2412@gmail.com>
2015-01-06 11:55:42 +1100

    Edit index.php

--- WebContent/index.php
+++ WebContent/index.php
@@ -1,11 +1,11 @@
 <?php
 
 $root = "E:/Project/UndergraduateThesis/SemanticConflictDetection/realData/DesignPatternsPHP";
-/*__VERSION__("B1", "$root/B0");
+__VERSION__("B1", "$root/B0");
 __VERSION__("B2", "$root/B1");
 __VERSION__("B3", "$root/B2");
 __VERSION__("B4", "$root/B3");
-__VERSION__("B5", "$root/B4");*/
+__VERSION__("B5", "$root/B4");
 
 $a = __CHECK__("$root/B0");
 echo "Total case: $a[nCase] </br>";
